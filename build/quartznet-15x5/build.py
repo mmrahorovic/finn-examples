95a94428de0e7fd3e51a4bfccae36f5d3d977a21
mmrahorovic <mmrahorovic@hotmail.com>
2023-01-06 09:26:52 +0000

    [quartznet]: initial changes

--- build/quartznet-15x5/build.py
+++ build/quartznet-15x5/build.py
@@ -2,7 +2,8 @@ import finn.builder.build_dataflow as build
 import finn.builder.build_dataflow_config as build_cfg
 from finn.util.basic import alveo_default_platform
 from custom_steps import (
-    step_quartznet_export
+    step_quartznet_export,
+    step_tidy_up
 )
 import os
 import shutil
@@ -11,11 +12,12 @@ import shutil
 model_name = "quartznet-15x5"
 
 # Platforms currently supported / tested on
-board = "U250"
+platform_name = "U250"
 vitis_platform = alveo_default_platform[board]
 synth_clk_period_ns = 5.0
 
 quartznet_build_steps = [
+        "step_quartznet_export",
         #"step_tidy_up",
         #"step_streamline",
         #"step_convert_to_hls",
@@ -34,16 +36,20 @@ quartznet_build_steps = [
         #"step_deployment_package",
 ]
 
+output_dir = "output_%s_%s" % (model_name, platform_name)
+os.makedirs(output_dir, exist_ok=True)
+
 cfg = build_cfg.DataflowBuildConfig(
     steps=quartznet_build_steps,
-    output_dir="output_%s_%s" % (model_name, board),
+    output_dir=output_dir,
     synth_clk_period_ns=synth_clk_period_ns,
-    board=board,
+    board=platform_name,
     shell_flow_type=build_cfg.ShellFlowType.VITIS_ALVEO,
     vitis_platform=vitis_platform,
     folding_config_file = folding_config_file,
+    auto_fifo_depths=True, # Enable for first round of FIFO sizing
     # enable extra performance optimizations (physopt)
-    #vitis_opt_strategy=build_cfg.VitisOptStrategyCfg.PERFORMANCE_BEST,
+    vitis_opt_strategy=build_cfg.VitisOptStrategyCfg.PERFORMANCE_BEST,
     generate_outputs=[
         build_cfg.DataflowOutputType.STITCHED_IP,
         build_cfg.DataflowOutputType.ESTIMATE_REPORTS,
@@ -56,6 +62,7 @@ cfg = build_cfg.DataflowBuildConfig(
 )
 
 step_quartznet_export(cfg, model_name)
-
 model_file = cfg.output_dir + "intermediate_models/%s_exported.onnx" % model_name
-build.build_dataflow_cfg(model_file, cfg)
+
+#model_file = cfg.output_dir + "end2end_quartznet_export_dev.onnx"
+#build.build_dataflow_cfg(model_file, cfg)
--- build/quartznet-15x5/custom_steps.py
+++ build/quartznet-15x5/custom_steps.py
@@ -1,20 +1,30 @@
 # other packages
-import brevitas.onnx as bo
-import brevitas_examples.speech_to_text as stt
+#import brevitas.onnx as bo
+#import brevitas_examples.speech_to_text as stt
 
 # general functions
-from finn.core.modelwrapper import ModelWrapper
-from finn.util.basic import make_build_dir, gen_finn_dt_tensor
-from finn.core.datatype import DataType
+from qonnx.core.modelwrapper import ModelWrapper
+from finn.util.basic import make_build_dir
+from qonnx.util.basic import gen_finn_dt_tensor
+from qonnx.core.datatype import DataType
+from finn.builder.build_dataflow_config import DataflowBuildConfig
 
 # transformations
-from finn.transformation.fold_constants import FoldConstants
-from finn.transformation.general import RemoveStaticGraphInputs
-from finn.transformation.infer_shapes import InferShapes
+from qonnx.transformation.fold_constants import FoldConstants
+from qonnx.transformation.general import RemoveStaticGraphInputs
+from qonnx.transformation.infer_shapes import InferShapes
+from qonnx.transformation.general import (
+    GiveUniqueNodeNames,
+    GiveRandomTensorNames,
+    GiveReadableTensorNames,
+    GiveUniqueParameterTensors
+)
+from qonnx.transformation.change_3d_tensors_to_4d import Change3DTo4DTensors
 
-def step_quartznet_export(cfg: DataflowBuildConfig, str: model_name):
+def step_quartznet_export(cfg: DataflowBuildConfig, model_name: str):
+    """ Export QuartzNet-15x5 model from Brevitas (4-bit weights & activations)"""
     # Export model from Brevitas
-    finn_onnx = cfg.output_dir + "/" + model_name "_exported.onnx"
+    finn_onnx = cfg.output_dir + "/" + model_name + "_exported.onnx"
     quartznet_torch = stt.quant_quartznet_perchannelscaling_4b(pretrained=True, export_mode=True)
     ishape = (1, 64, 256)
     idt = DataType["FLOAT32"]
@@ -29,3 +39,14 @@ def step_quartznet_export(cfg: DataflowBuildConfig, str: model_name):
     assert len(model.graph.output) == 1
 
     model.save(cfg.output_dir + "/intermediate_models/" + model_name + "_exported.onnx")
+
+#def step_tidy_up(model: ModelWrapper, cfg: DataflowBuildConfig):
+#    """ Run the tidy-up step on the QuartzNet model; e.g. shape and datatype inference, constant folding, inferring node names, 3D to 4D node/tensor transformation"""
+#    model = model.transform(GiveUniqueNodeNames())
+#    model = model.transform(GiveRandomTensorNames())
+#    model = model.transform(GiveReadableTensorNames())
+#    model = model.transform(GiveUniqueParameterTensors())
+#    model = model.transform(Change3DTo4DTensors())
+
+#    return model
+
